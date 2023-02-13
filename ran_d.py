28fd3d757f1e3858398f02aad0cbca7f1ed94cd0
Godfrey Gudah <gugod254@gmail.com>
2023-02-14 01:00:46 +0300

    Update ran_d.py
    rounds

--- ran_d.py
+++ ran_d.py
@@ -17,11 +17,31 @@ def update_odds(odds):
         odds[player] += new_odd
     return odds
 
+def simulate_round(odds):
+    round_results = {}
+    for player in odds:
+        # Simulate a choice made by each player in the round
+        choice = random.choice(odds[player])
+        round_results[player] = choice
+    return round_results
+
+def determine_winner(round_results):
+    # Determine the player with the highest number in the round
+    winner = max(round_results, key=round_results.get)
+    return winner
+
 while True:
     # Call the update_odds function to add new odds for each player
     odds = update_odds(odds)
+    # Simulate a round
+    round_results = simulate_round(odds)
+    # Determine the winner of the round
+    winner = determine_winner(round_results)
+    print(f"Round One:")
+    for player in round_results:
+        print(f"{player} - {round_results[player]}")
+    print(f"{winner} won the round\n")
     # Check if the user wants to exit
     exit = input("Press 'q' to exit or any other key to continue: ")
     if exit == 'q':
         break
-
